8cad1369ef2d7448d0dc663aa834daadd734cac4
Raphael Bigot <contact@raphael-bigot.info>
2018-10-28 14:04:43 +0100

    feat: Score management Added a counter. Improved the JS. Main css structure for the cards

--- src/assets/js/memory.js
+++ src/assets/js/memory.js
@@ -19,6 +19,11 @@ function shuffle(array) {
 }
 
 //============================----------------------------
+//store the attempts number
+let attemptsNumber;
+
+//store the successfull attempts
+let score;
 
 const memory = document.querySelector('.cards');
 
@@ -31,7 +36,48 @@ let shuffledCards = Array.prototype.slice.call(allCards);
 //used for performances (don't append each li one by one)
 const fragment = document.createDocumentFragment();
 
+//The buttons for each cards with event listeners
+const cardBtn = document.querySelectorAll('.card__button');
+
+let counter = document.querySelector('.counterNumber');
+
+const pickCard = function(e) {
+  const _this = e.target;
+  //we store the card in the array
+  chosenCards.push(_this);
+  // we prevent from chosing again that card
+  _this.removeEventListener('click', pickCard);
+
+  if (chosenCards.length === 2) {
+    //when 2 cards have been picked, if the text of the elements are the same...
+    if (chosenCards[0].outerText === chosenCards[1].outerText) {
+      console.log('a pair !');
+      score ++;
+    } else {
+      console.log('try again...');
+      //If not, the 2 cards are selectable for a new try
+      for (let chosenCard of chosenCards) {
+        chosenCard.addEventListener('click', pickCard);
+      }
+    } // and the array is empty
+      chosenCards = [];
+      attemptsNumber ++;
+      console.log('counter : ', counter);
+      counter.textContent = attemptsNumber;
+  }
+
+  // when all pairs have been successfully revealed, the game is over
+  if (score === shuffledCards.length / 2) {
+    console.log('You win the game !!!');
+  }
+};
+
+
 const launchGame = function() {
+  score = 0;
+  attemptsNumber = 0;
+  console.log('score : ', score);
+
   //shuffle the li elements
   shuffledCards = shuffle(shuffledCards);
   for (let shuffledCard of shuffledCards) {
@@ -39,6 +85,12 @@ const launchGame = function() {
   }
   //appends reordered li elements in ul
   memory.appendChild(fragment);
+  counter.textContent = 0;
+
+  //adds the function to the button of every cards
+  for (i = 0; i < cardBtn.length; i ++) {
+    cardBtn[i].addEventListener('click', pickCard);
+  }
 }
 
 //Reset game button
@@ -51,32 +103,4 @@ launchGame();
 //The array storing the 2 chosen cards at each try
 let chosenCards = [];
 
-//The buttons for each cards with event listeners
-const cardBtn = document.querySelectorAll('.card__button');
 
-const pickCard = function(e) {
-  const _this = e.target;
-  //we store the card in the array
-  chosenCards.push(_this);
-  // we prevent from chosing again that card
-  _this.removeEventListener('click', pickCard);
-
-  if (chosenCards.length === 2) {
-    //when 2 cards have been picked, if the text of the elements are the same...
-    if (chosenCards[0].outerText === chosenCards[1].outerText) {
-      console.log('won !!!');
-    } else {
-      console.log('lost...');
-      //If not, the 2 cards are selectable for a new try
-      for (let chosenCard of chosenCards) {
-        chosenCard.addEventListener('click', pickCard);
-      }
-    } // and the array is empty
-      chosenCards = [];
-  }
-};
-
-//adds the function to the button of every cards
-for (i = 0; i < cardBtn.length; i ++) {
-  cardBtn[i].addEventListener('click', pickCard);
-}

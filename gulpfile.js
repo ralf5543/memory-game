667b9c4ade644c583a99c52911440834d0007524
Raphaël Bigot <raphael.bigot@fullsix.com>
2018-10-29 17:43:15 +0100

    feat: animations
    added css animations for revealed cards, and stars counter

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@
 // Required
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
 var autoprefixer = require('gulp-autoprefixer');
+var responsive = require('gulp-responsive');
 
 // ======================------------------ TASKS
 
@@ -13,6 +13,56 @@ gulp.task('css', function () {
       .pipe(sass().on('error', sass.logError))//Convert sass files to css files
       .pipe(autoprefixer())//adds prefix for old browsers
       .pipe(gulp.dest('./dist/assets/css'));
+});
+
+//Generates images at different sizes
+gulp.task('images', function () {
+
+  return gulp.src('./sources/*.{jpg,png}')
+      .pipe(responsive({
+
+        '*': [{
+          width: 200,
+          height: 200,
+          rename: { suffix: '-200px' }
+        }, {
+          width: 200 * 2,
+          height: 200 * 2,
+          rename: { suffix: '-200px_2x' }
+        }, {
+          width: 200 * 3,
+          height: 200 * 3,
+          rename: { suffix: '-200px_3x' }
+        }, {
+          width: 300,
+          height: 300,
+          rename: { suffix: '-300px' }
+        }, {
+          width: 300 * 2,
+          height: 300 * 2,
+          rename: { suffix: '-300px_2x' }
+        }, {
+          width: 300 * 3,
+          height: 300 * 3,
+          rename: { suffix: '-300px_3x' }
+        }, {
+          // Compress, strip metadata, and rename original image
+          rename: { suffix: '-original' }
+        }],
+      }, {
+        // Global configuration for all images
+        // The output quality for JPEG, WebP and TIFF output formats
+        quality: 70,
+        // Use progressive (interlace) scan for JPEG and PNG output
+        progressive: true,
+        // Strip all metadata
+        withMetadata: false,
+        withoutEnlargement: true,
+        skipOnEnlargement: true, // that option copy original file with/without renaming
+        errorOnEnlargement: false,
+        crop: 'center'
+      }))
+      .pipe(gulp.dest('./dist/assets/img'));
 });
 
 //Update css files each time a sass file is modified
--- src/assets/js/memory.js
+++ src/assets/js/memory.js
@@ -18,7 +18,20 @@ function shuffle(array) {
   return array;
 }
 
-//============================----------------------------
+//==============------------------ MODAL
+const modal = document.querySelector('.js-modal');
+const toggleModal = function () {
+  modal.classList.toggle('is-closed');
+};
+
+const modalClosersClass = document.querySelectorAll('.js-closeModal');
+let modalClosers = Array.prototype.slice.call(modalClosersClass);
+
+for (modalCloser of modalClosers) {
+  modalCloser.addEventListener('click', toggleModal);
+}
+
+
 //store the attempts number
 let attemptsNumber;
 
@@ -41,8 +54,13 @@ const cardBtn = document.querySelectorAll('.card__button');
 
 let counter = document.querySelector('.counterNumber');
 
-const pickCard = function(e) {
-  const _this = e.target;
+let movesResults = document.querySelector('.modal__text__moves');
+
+//
+let stars = 'rrr';
+
+const pickCard = function (e) {
+  const _this = e.currentTarget;
 
   //the li containing the button toggles a class to "return" visually the card
   _this.parentNode.classList.toggle('is-revealed');
@@ -52,34 +70,68 @@ const pickCard = function(e) {
   // we prevent from chosing again that card
   _this.removeEventListener('click', pickCard);
 
+  console.log('parentNode : ', _this.parentNode);
+  console.log('e.target : ', e.currentTarget);
+
   if (chosenCards.length === 2) {
     //when 2 cards have been picked, if the text of the elements are the same...
     if (chosenCards[0].outerText === chosenCards[1].outerText) {
       console.log('a pair !');
-      score ++;
+      for (let chosenCard of chosenCards) {
+        chosenCard.parentNode.classList.add('is-correct');
+      }
+      score++;
     } else {
       console.log('try again...');
       //If not, the 2 cards are selectable for a new try...
       for (let chosenCard of chosenCards) {
+        chosenCard.parentNode.classList.toggle('is-wrong');
         chosenCard.addEventListener('click', pickCard);
         //...and they're returned after 3s
-        setTimeout(function (){chosenCard.parentNode.classList.toggle('is-revealed')}, 3000);
+        setTimeout(function () {
+          chosenCard.parentNode.classList.toggle('is-revealed');
+          chosenCard.parentNode.classList.toggle('is-wrong');
+        }, 3000);
+
       }
     } // and the array is empty
-      chosenCards = [];
-      attemptsNumber ++;
-      console.log('counter : ', counter);
-      counter.textContent = attemptsNumber;
+    chosenCards = [];
+    attemptsNumber++;
+    console.log('counter : ', counter);
+    counter.textContent = attemptsNumber;
+
+    if (attemptsNumber <= (allCards.length / 2)) {
+      stars = 5;
+    } else if (attemptsNumber <= ((allCards.length / 2) + 2)) {
+      stars = 4;
+      console.log(4);
+    } else if (attemptsNumber <= ((allCards.length / 2) + 4)) {
+      stars = 3;
+    } else if (attemptsNumber <= ((allCards.length / 2) + 6)) {
+      stars = 2;
+    } else if (attemptsNumber <= ((allCards.length / 2) + 8)) {
+      stars = 1;
+    } else {
+      stars = 0
+    }
+
+    document.querySelector('.stars').textContent = stars;
   }
 
   // when all pairs have been successfully revealed, the game is over
   if (score === shuffledCards.length / 2) {
     console.log('You win the game !!!');
+    document.querySelector('.modal__text__stars').textContent = stars;
+    if (stars >= 2) {
+      document.querySelector('.modal__text__starsPlural').textContent = 's';
+    }
+    movesResults.textContent = attemptsNumber;
+    toggleModal();
   }
 };
 
 
-const launchGame = function() {
+const launchGame = function () {
   score = 0;
   attemptsNumber = 0;
   console.log('score : ', score);
@@ -87,6 +139,8 @@ const launchGame = function() {
   //shuffle the li elements
   shuffledCards = shuffle(shuffledCards);
   for (let shuffledCard of shuffledCards) {
+    //remove win indications from last game
+    shuffledCard.classList.remove('is-correct', 'is-wrong', 'is-revealed');
     fragment.appendChild(shuffledCard);
   }
   //appends reordered li elements in ul
@@ -94,19 +148,26 @@ const launchGame = function() {
   counter.textContent = 0;
 
   //adds the function to the button of every cards
-  for (i = 0; i < cardBtn.length; i ++) {
+  for (i = 0; i < cardBtn.length; i++) {
     cardBtn[i].addEventListener('click', pickCard);
   }
 }
 
 //Reset game button
-const resetBtn = document.querySelector('.reset');
-resetBtn.addEventListener('click', launchGame);
+//target all button elements with 'js-reset' class
+const resetBtnsClass = document.querySelectorAll('.js-reset');
+
+//change the NodeList returned to a real array
+let resetBtns = Array.prototype.slice.call(resetBtnsClass);
+
+for (let resetBtn of resetBtns) {
+  resetBtn.addEventListener('click', launchGame);
+}
+
 
 //init
 launchGame();
 
 //The array storing the 2 chosen cards at each try
 let chosenCards = [];
-
 
